713b5ca4963605de9762fb814fc9b89b43a0b45e
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 19:26:50 +0100

    Merging two tests into test cases

--- BowlingKata.Tests/BowlingShould.cs
+++ BowlingKata.Tests/BowlingShould.cs
@@ -4,26 +4,17 @@ using NUnit.Framework;
 namespace BowlingKata.Tests
 {
     [TestFixture]
-    public class BowlingShould  
+    public class BowlingShould
     {
-        [Test]
-        public void return_zero_for_all_gutter_game()
+        [TestCase("--|--|--|--|--|--|--|--|--|--|", 0)]
+        [TestCase("1-|--|--|--|--|--|--|--|--|--|", 1)]
+        public void return_zero_for_all_gutter_game(string game, int expectedScore)
         {
             var bowling = new Bowling();
 
-            var score = bowling.CalculateScore("--|--|--|--|--|--|--|--|--|--|");
+            var score = bowling.CalculateScore(game);
 
-            Assert.That(score, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void return_one_for_one_pin_hit()
-        {
-            var bowling=new Bowling();
-
-            var score = bowling.CalculateScore("1-|--|--|--|--|--|--|--|--|--|");
-
-            Assert.That(score, Is.EqualTo(1));
+            Assert.That(score, Is.EqualTo(expectedScore));
         }
     }
 }
