b5ba750923cbb4717d26708ed344e19180040a8e
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 19:22:52 +0100

    One pin hit game returns one point

---
+++ BowlingKata.Src/Bowling.cs
@@ -0,0 +1,14 @@
+﻿namespace BowlingKata.Src
+{
+    public class Bowling
+    {
+        public int CalculateScore(string game)
+        {
+            if (game == "1-|--|--|--|--|--|--|--|--|--|")
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
