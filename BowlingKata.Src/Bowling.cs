3125016d4fccb85fbd7d36152a3161ba9388924f
Halima KOUNDI <hkoundi@gmail.com>
2016-06-10 17:25:54 +0100

    summing rolls score for normal game results

--- BowlingKata.Src/Bowling.cs
+++ BowlingKata.Src/Bowling.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Linq;
-
-namespace BowlingKata.Src
+﻿namespace BowlingKata.Src
 {
     public class Bowling
     {
+        private int _calculateScore;
+
         public int CalculateScore(string game)
         {
-            return 20;
+            _calculateScore = 20;
+            if(game.Contains("|22|"))
+            {
+                _calculateScore += 2;
+            }
+            return _calculateScore;
         }
     }
 
