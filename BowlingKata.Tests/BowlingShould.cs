f0ad10cc9f84ca58c09821adce51293da3c4ab2f
Halima KOUNDI <hkoundi@gmail.com>
2016-06-13 12:01:28 +0100

    get score of games with all strikes

--- BowlingKata.Tests/BowlingShould.cs
+++ BowlingKata.Tests/BowlingShould.cs
@@ -17,6 +17,7 @@ namespace BowlingKata.Tests
         [TestCase("X|52|11|11|11|11|11|11|11|11||", 40 )]
         [TestCase("X|X|X|11|11|11|11|11|11|11||", 77 )]
         [TestCase("X|7/|9-|X|-8|8/|-6|X|X|X||81", 167 )]
+        [TestCase("X|X|X|X|X|X|X|X|X|X||XX", 300)]
         public void return_game_score(string game, int expectedScore)
         {
             var bowling = new Bowling();
