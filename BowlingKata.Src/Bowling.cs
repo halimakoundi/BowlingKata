d34d9b7286b085dc4c145f788d3ad64e49a499fd
Halima KOUNDI <hkoundi@gmail.com>
2016-06-13 17:11:23 +0100

    extracting method to calculate the score for all pins down frame

--- BowlingKata.Src/Bowling.cs
+++ BowlingKata.Src/Bowling.cs
@@ -18,27 +18,34 @@ namespace BowlingKata.Src
             {
                 var frame = _frames[i];
                 _gameScore += frame.Score();
-                if (frame.Score() == 10)
-                {
-                    var nextFrame = !frame.IsLastFrame() ? _frames[i + 1]
-                    : BonusRolls(game);
-                    _gameScore += nextFrame.GetRollScore(0);
-                    if (frame.IsStrike())
-                    {
-                        if (nextFrame.IsStrike())
-                        {
-                            var secondNextFrame = frame.IsOneBeforeLastFrame() ? BonusRolls(game) : _frames[i + 2];
-                            _gameScore += secondNextFrame.GetRollScore(0);
-                        }
-                        else
-                        {
-                            _gameScore += nextFrame.GetRollScore(1);
-                        }
-                    }
-                }
-                
+                _gameScore += GetAllPinsDownFrameScore(game, frame, i);
             }
             return _gameScore;
+        }
+
+        private int GetAllPinsDownFrameScore(string game, Frame frame, int i)
+        {
+            var frameScore = 0;
+            if (frame.Score() == 10)
+            {
+                var nextFrame = !frame.IsLastFrame()
+                    ? _frames[i + 1]
+                    : BonusRolls(game);
+                frameScore = nextFrame.GetRollScore(0);
+                if (frame.IsStrike())
+                {
+                    if (nextFrame.IsStrike())
+                    {
+                        var secondNextFrame = frame.IsOneBeforeLastFrame() ? BonusRolls(game) : _frames[i + 2];
+                        frameScore += secondNextFrame.GetRollScore(0);
+                    }
+                    else
+                    {
+                        frameScore += nextFrame.GetRollScore(1);
+                    }
+                }
+            }
+            return frameScore;
         }
 
         private static Frame BonusRolls(string game)
