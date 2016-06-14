d4386560f96b2bbb8b6f3c87f78e747ac9eb853b
Halima KOUNDI <hkoundi@gmail.com>
2016-06-14 17:04:24 +0100

    introducing second next roll score in strike frame

--- BowlingKata.Src/StrikeFrame.cs
+++ BowlingKata.Src/StrikeFrame.cs
@@ -3,12 +3,16 @@
     internal class StrikeFrame : Frame
     {
         private readonly Roll _nextRollResults;
+        private Roll _secondNextRollScore;
 
         public StrikeFrame(string rollsResult, int index, int gameLength, string[] gameResults, Roll[] bonusRolls) : base(rollsResult, index, gameLength)
         {
             _nextRollResults = !IsLastFrame() ?
-                               new Roll(gameResults[index + 1], 0)
-                               : bonusRolls[0];
+                                    new Roll(gameResults[index + 1], 0)
+                                    : bonusRolls[0];
+            _secondNextRollScore = !IsLastFrame() ?
+                                    new Roll(gameResults[index + 1], 1)
+                                    : bonusRolls[1];
             PopulateNextRollScore();
         }
 
@@ -19,6 +23,7 @@
                 new Roll(RollsResult, 0),
                 new Roll(RollsResult, 1),
                 _nextRollResults,
+                _secondNextRollScore
             };
 
         }
